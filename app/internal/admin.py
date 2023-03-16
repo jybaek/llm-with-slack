7eee049338b579e3e8d0b82839ae23c89e01bcdd
Caley Baek <jybaek@madup.com>
2023-03-17 02:39:06 +0900

    update admin

--- app/internal/admin.py
+++ app/internal/admin.py
@@ -1,8 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from app.models.redis import RedisClient
 
 router = APIRouter()
 
 
-@router.post("/")
-async def update_admin():
-    return {"message": "Admin getting schwifty"}
+@router.post("/user-cleanup")
+async def update_admin(
+    redis_client: RedisClient = Depends(RedisClient),
+    user_id: str = "u_1234",
+):
+    redis_conn = redis_client.get_conn()
+    redis_conn.delete(user_id)
+    return "Clean up done"
+
+
+@router.post("/all-cleanup")
+async def update_admin(
+    redis_client: RedisClient = Depends(RedisClient),
+):
+    redis_conn = redis_client.get_conn()
+    redis_conn.flushdb()
+    return "Clean up done"
