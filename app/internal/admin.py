0be0876d7e436bfe63a6b6c1615a260ea6cccd0d
Caley Baek <jybaek@madup.com>
2023-03-17 02:51:29 +0900

    Add get_user_message

--- app/internal/admin.py
+++ app/internal/admin.py
@@ -6,7 +6,7 @@ router = APIRouter()
 
 
 @router.post("/user-cleanup")
-async def update_admin(
+async def user_cleanup(
     redis_client: RedisClient = Depends(RedisClient),
     user_id: str = "u_1234",
 ):
@@ -15,8 +15,18 @@ async def update_admin(
     return "Clean up done"
 
 
+@router.post("/get-user-message")
+async def user_cleanup(
+    redis_client: RedisClient = Depends(RedisClient),
+    user_id: str = "u_1234",
+):
+    redis_conn = redis_client.get_conn()
+    return redis_conn.lrange(user_id, 0, -1)
+
+
+
 @router.post("/all-cleanup")
-async def update_admin(
+async def all_cleanup(
     redis_client: RedisClient = Depends(RedisClient),
 ):
     redis_conn = redis_client.get_conn()
