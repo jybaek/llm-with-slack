40a7f90e91e5f1127969ffabdc4d136272087471
JB <jybaek@madup.com>
2024-05-24 18:44:07 +0900

    feat: Add API endpoint for random model selection

--- app/routers/slack.py
+++ app/routers/slack.py
@@ -1,3 +1,5 @@
+import random
+
 from fastapi import APIRouter
 from starlette.background import BackgroundTasks
 from starlette.responses import Response
@@ -25,4 +27,14 @@ async def slack(message: dict, background_tasks: BackgroundTasks):
 
     # Because Slack is constrained to give a response in 3 seconds, ChatGPT processing is handled by background_tasks.
     background_tasks.add_task(message_process, message, LLMModel.GEMINI)
+    return Response("ok")
+
+@router.post("/random")
+async def slack(message: dict, background_tasks: BackgroundTasks):
+    if message.get("challenge"):
+        return message.get("challenge")
+
+    llm_model = random.choices([LLMModel.GPT, LLMModel.GEMINI])
+    # Because Slack is constrained to give a response in 3 seconds, ChatGPT processing is handled by background_tasks.
+    background_tasks.add_task(message_process, message, llm_model)
     return Response("ok")
